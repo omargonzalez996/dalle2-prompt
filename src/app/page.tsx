519266226d83071c80416740d7e72edbc506f3ea
omargonzalez996 <omar.gonz996@gmail.com>
2023-07-07 15:58:26 -0600

    amonos que aqui espantan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,27 @@ import Image from "next/image";
 function Page() {
   const [prompt, setPrompt] = useState("");
 
+  const Submitter = async (e: any) => {
+    e.preventDefault();
+    console.log(prompt);
+    const res = await fetch("/api/generate", {
+      method: "POST",
+      body: JSON.stringify({ prompt }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div id="main-cont">
       <div id="logo-container">
         <Image id="logo" src={logo} alt="logo" />
       </div>
       <div id="form-container">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log(prompt);
-          }}
-          id="formulario"
-        >
+        <form onSubmit={(e) => Submitter(e)} id="formulario">
           <textarea
             id="input-prompt"
             placeholder="escribe tu prompt"
